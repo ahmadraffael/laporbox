7e0ca42646a069318ad70ce2f18d227a5786385b
ahmadraffael <ahmadrf921@gmail.com>
2024-12-06 20:16:49 +0700

    update sql

--- db/e-report.sql
+++ db/e-report.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 04 Nov 2024 pada 04.32
--- Versi server: 10.4.32-MariaDB
--- Versi PHP: 8.2.12
+-- Generation Time: Dec 06, 2024 at 02:16 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.0.30
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,7 +24,7 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `reports`
+-- Table structure for table `reports`
 --
 
 CREATE TABLE `reports` (
@@ -39,16 +39,19 @@ CREATE TABLE `reports` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data untuk tabel `reports`
+-- Dumping data for table `reports`
 --
 
 INSERT INTO `reports` (`id`, `title`, `slug`, `content`, `thumbnail`, `status`, `user_id`, `created_at`) VALUES
-(1, 'afas', '', '&amp;lt;p&amp;gt;&amp;lt;strong&amp;gt;asfas&amp;lt;/strong&amp;gt;&amp;lt;/p&amp;gt;', '', 0, 0, '2024-11-03 14:26:18');
+(7, 'kami laparrr', 'kami-laparrr', '&amp;lt;p&amp;gt;aaaaaaaa&amp;lt;/p&amp;gt;', '6752a32a1406e.png', 1, 10, '2024-12-06 07:09:30'),
+(8, 'pakett', 'pakett', '&amp;lt;p&amp;gt;paket 1 kuintal senter telah kami kirimkan didepan rumah anda&amp;lt;/p&amp;gt;', '6752c595bdf18.png', 1, 12, '2024-12-06 09:36:21'),
+(9, 'senter lagi', 'senter-lagi', '&amp;lt;p&amp;gt;maaf pak. senternya kebawa satu. ini saya kembalik kedepan rumah anda&amp;lt;/p&amp;gt;', '6752c5c12913e.png', 0, 12, '2024-12-06 09:37:05'),
+(10, 'wayang jogja', 'wayang-jogja', '&amp;lt;p&amp;gt;&amp;lt;strong&amp;gt;WAYANG EKSLUSIF SUDAH TIBA!&amp;lt;/strong&amp;gt;&amp;lt;/p&amp;gt;&amp;lt;p&amp;gt;&amp;amp;nbsp;&amp;lt;/p&amp;gt;&amp;lt;p&amp;gt;Wayang eksklusif dari kulit raja jin telah tiba dikediaman anda&amp;lt;/p&amp;gt;', '6752c617d7a14.jpg', 0, 12, '2024-12-06 09:38:31');
 
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `roles`
+-- Table structure for table `roles`
 --
 
 CREATE TABLE `roles` (
@@ -57,7 +60,7 @@ CREATE TABLE `roles` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data untuk tabel `roles`
+-- Dumping data for table `roles`
 --
 
 INSERT INTO `roles` (`id`, `name`) VALUES
@@ -68,7 +71,7 @@ INSERT INTO `roles` (`id`, `name`) VALUES
 -- --------------------------------------------------------
 
 --
--- Struktur dari tabel `users`
+-- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
@@ -82,31 +85,34 @@ CREATE TABLE `users` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
--- Dumping data untuk tabel `users`
+-- Dumping data for table `users`
 --
 
 INSERT INTO `users` (`id`, `username`, `nik`, `name`, `phone_number`, `password`, `role_id`) VALUES
-(6, 'superadmin', '111111', 'Super admin', '111111', '$2y$10$S6/qf3TPYCUgEtzRt4AT/.G0hT9oC9YhPNyT.mKNOw2cIkdsE3YSy', 1),
-(8, 'petugas', '222222', 'Petugas', '222222', '$2y$10$6w6UfBZ4Y.idavfLe.OxIuT5ke1AWv4EWrirkbo8t6wCdYi1gf8oW', 2);
+(8, 'petugas', '222222', 'Petugas', '222222', '$2y$10$6w6UfBZ4Y.idavfLe.OxIuT5ke1AWv4EWrirkbo8t6wCdYi1gf8oW', 2),
+(10, 'abc', '2134565', 'abc', '628128898121', '$2y$10$LUoTYZT1svB6f9g4nC6xTeHJRLiY2pvkqoBZ9wyaegtrvXd08zIAi', 3),
+(11, 'mimin', '123476', 'Ahmad Raffael Fauzan', '081288981214', '$2y$10$jARhYEzdmrpaVY5dLRzYAenDVciCMALciZv1vAXAoI.U2tAdwbaHq', 1),
+(12, 'khalil', '198723', 'khalil bro', '098213532', '$2y$10$EZj/5t.BJgPkZANiqG6i5ui6EC6W2wH7jJsqoLyAcJaLSTG07c0ku', 3),
+(13, 'pepetugas', '98737121', 'John Chine', '081276751', '$2y$10$OCwLFn0GMrkUyj9Jq78gReHgvhCRegszEZ8.Ny48Q1TDK01jLq/XO', 2);
 
 --
 -- Indexes for dumped tables
 --
 
 --
--- Indeks untuk tabel `reports`
+-- Indexes for table `reports`
 --
 ALTER TABLE `reports`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `roles`
+-- Indexes for table `roles`
 --
 ALTER TABLE `roles`
   ADD PRIMARY KEY (`id`);
 
 --
--- Indeks untuk tabel `users`
+-- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
@@ -114,33 +120,33 @@ ALTER TABLE `users`
   ADD KEY `role_id` (`role_id`);
 
 --
--- AUTO_INCREMENT untuk tabel yang dibuang
+-- AUTO_INCREMENT for dumped tables
 --
 
 --
--- AUTO_INCREMENT untuk tabel `reports`
+-- AUTO_INCREMENT for table `reports`
 --
 ALTER TABLE `reports`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=11;
 
 --
--- AUTO_INCREMENT untuk tabel `roles`
+-- AUTO_INCREMENT for table `roles`
 --
 ALTER TABLE `roles`
   MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 
 --
--- AUTO_INCREMENT untuk tabel `users`
+-- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
--- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
+-- Constraints for dumped tables
 --
 
 --
--- Ketidakleluasaan untuk tabel `users`
+-- Constraints for table `users`
 --
 ALTER TABLE `users`
   ADD CONSTRAINT `users_ibfk_1` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`);
